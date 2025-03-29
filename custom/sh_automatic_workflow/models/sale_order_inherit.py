3ab7a37e90c51d7f6fc7239de66cdf837b2edff9
Taha Musani <taha@softhealer.com>
2025-03-29 14:20:23 +0530

    [ADD] sale methods [remaining] payment method and send mail

--- custom/sh_automatic_workflow/models/sale_order_inherit.py
+++ custom/sh_automatic_workflow/models/sale_order_inherit.py
@@ -4,26 +4,36 @@ from odoo import fields, models, api
 class sh_sale_order_inherit(models.Model):
     _inherit = "sale.order"
     _description = "sales note details"
-    
+
     def default_get(self, fields):
         res = super(sh_sale_order_inherit, self).default_get(fields)
-        
+
         # Get context values
-        res_enable = self.env['ir.config_parameter'].sudo().get_param("sh_automatic_workflow.enable_automatic_workflow")
-        res_auto_id = self.env['ir.config_parameter'].sudo().get_param("sh_automatic_workflow.automatic_workflow_id")
-        
+        res_enable = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("sh_automatic_workflow.enable_automatic_workflow")
+        )
+        res_auto_id = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("sh_automatic_workflow.automatic_workflow_id")
+        )
+
         if res_enable:
-            res['enable_automatic_workflow'] = res_enable
+            res["enable_automatic_workflow"] = res_enable
         else:
-            res['enable_automatic_workflow'] = False
-        
+            res["enable_automatic_workflow"] = False
+
         if res_auto_id:
             res_auto_id = int(res_auto_id)
             res_auto_id_record = self.env["sh.automatic.workflow"].browse(res_auto_id)
             if res_auto_id_record:
-                res['automatic_workflow_id'] = res_auto_id_record.id
-        
-        print('\n\n\n-----automatic_workflow_id------->', res.get("automatic_workflow_id"))
+                res["automatic_workflow_id"] = res_auto_id_record.id
+
+        print(
+            "\n\n\n-----automatic_workflow_id------->", res.get("automatic_workflow_id")
+        )
         return res
 
     enable_automatic_workflow = fields.Boolean(
@@ -34,22 +44,47 @@ class sh_sale_order_inherit(models.Model):
         string="Automatic Workflow",
     )
 
-
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def _onchange_partner_id(self):
         if self.partner_id:
             if self.partner_id.automatic_workflow_id:
-                self.automatic_workflow_id=self.partner_id.automatic_workflow_id
+                self.automatic_workflow_id = self.partner_id.automatic_workflow_id
             else:
-                res_auto_id = self.env['ir.config_parameter'].sudo().get_param("sh_automatic_workflow.automatic_workflow_id")
-                self.automatic_workflow_id=int(res_auto_id)
-                
-                
+                res_auto_id = (
+                    self.env["ir.config_parameter"]
+                    .sudo()
+                    .get_param("sh_automatic_workflow.automatic_workflow_id")
+                )
+                self.automatic_workflow_id = int(res_auto_id)
+
     def action_confirm(self):
         super().action_confirm()
         if self.automatic_workflow_id:
             if self.automatic_workflow_id.delivery_order:
-                rec=self.env["stock.picking"].search([("sale_id","=",self.id)])
-                print("============rec=========>",rec)
-                print("============rec=========>",self.id)
-                rec.button_validate()
+                rec = self.env["stock.picking"].search([("sale_id", "=", self.id)])
+                print('\n\n\n-----rec------->',rec)
+                val=rec.button_validate()
+                print('\n\n\n-----val------->',val)
+
+            if self.automatic_workflow_id.create_invoice:
+                invoice_created = self._create_invoices()
+
+                if self.automatic_workflow_id.validate_invoice:
+                    invoice_created.action_post()
+
+                    if self.automatic_workflow_id.send_invo_by_email:
+                        invoice_created.action_invoice_sent()
+                        invoice_created.action_send_and_print()
+
+                    if self.automatic_workflow_id.register_payment:
+                        return (
+                            self.env["account.payment.register"]
+                            .with_context(
+                                active_model="account.move",
+                                active_ids=invoice_created.ids,
+                                default_payment_method_line_id=self.automatic_workflow_id.sh_payment_method_id.id,
+                                default_journal_id=self.automatic_workflow_id.payment_journal_id.id,
+                            )
+                            .create({"group_payment": False})
+                            .action_create_payments()
+                        )
--- custom/sh_automatic_workflow/models/sh_automatic_workflow.py
+++ custom/sh_automatic_workflow/models/sh_automatic_workflow.py
@@ -7,7 +7,6 @@ class sh_automatic_workflow(models.Model):
 
     name = fields.Char(string='Name')
     delivery_order = fields.Boolean(string='Delivery Order')
-    force_transfer = fields.Boolean(string='Force Transfer')
     create_invoice = fields.Boolean(string='Create Invoice')
     validate_invoice = fields.Boolean(string='Validate Invoice')
     register_payment = fields.Boolean(string='Register Payment')
@@ -21,63 +20,11 @@ class sh_automatic_workflow(models.Model):
         'account.journal',
         string='Payment Journal',
         )
-    payment_method_id = fields.Many2one(
-        'account.journal',
+    sh_payment_method_id = fields.Many2one(
+        'account.payment.method',
         string='Payment Method',
         )
     company_id = fields.Many2one(
         'res.company',
         string='Company',
         )
-    
-    
-    
-    # @api.constrains("force_transfer")
-    # def check_force_transfer(self):
-    #     for rec in self:
-    #         print('\n\n\n-----rec.force_transfer------->',rec.force_transfer)
-    #         print('\n\n\n-----rec.delivery_order------->',rec.delivery_order)
-    #         if rec.force_transfer:
-    #             if not rec.delivery_order:
-    #                 raise ValidationError("You can not select force transfer without selecting delivery order")
-                
-    # @api.constrains("create_invoice")
-    # def check_create_invoice(self):
-    #     for rec in self:
-    #         print('\n\n\n-----rec.force_transfer------->',rec.force_transfer)
-    #         print('\n\n\n-----rec.delivery_order------->',rec.delivery_order)
-    #         if rec.create_invoice:
-    #             if not rec.force_transfer:
-    #                 raise ValidationError("You can not select create invoice without selecting force transfer")
-                
-    # @api.constrains("validate_invoice")
-    # def check_validate_invoice(self):
-    #     for rec in self:
-    #         print('\n\n\n-----rec.force_transfer------->',rec.force_transfer)
-    #         print('\n\n\n-----rec.delivery_order------->',rec.delivery_order)
-    #         if rec.validate_invoice:
-    #             if not rec.create_invoice:
-    #                 raise ValidationError("You can not select validate invoice without selecting create invoice")
-
-    # @api.constrains("register_payment")
-    # def check_register_payment(self):
-    #     for rec in self:
-    #         print('\n\n\n-----rec.force_transfer------->',rec.force_transfer)
-    #         print('\n\n\n-----rec.delivery_order------->',rec.delivery_order)
-    #         if rec.register_payment:
-    #             if not rec.validate_invoice:
-    #                 raise ValidationError("You can not select register payment without selecting validate invoice")
-
-    # @api.constrains("send_invo_by_email")
-    # def check_send_invo_by_email(self):
-    #     for rec in self:
-    #         print('\n\n\n-----rec.force_transfer------->',rec.force_transfer)
-    #         print('\n\n\n-----rec.delivery_order------->',rec.delivery_order)
-    #         if rec.send_invo_by_email:
-    #             if not rec.register_payment:
-    #                 raise ValidationError("You can not select send invoice by email without selecting register payment")
-    
-    # def write(self, vals):
-    #     print('\n\n\n-----values------->',vals)
-    #     res = super(sh_automatic_workflow,self).write(vals)
-    #     return res
